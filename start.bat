e35ae013a2460a7cc62454fc3e67d86cff671b5d
ehrrsn7 <ehrrsn7@gmail.com>
2024-07-29 14:47:18 -0700

    server changes

--- start.bat
+++ start.bat
@@ -1,2 +1,7 @@
 @REM ensure JAVA_HOME points to a Java SE v22+ installation (I.E. "C:\Program Files\Java\jdk-22\bin\java.exe")
+
+@REM Opening Gateway to port 25565 using ngrok
+@REM ngrok tcp 25565
+
+@REM Running Server...
 %JAVA_HOME% -Xmx4G -jar server.jar nogui;
